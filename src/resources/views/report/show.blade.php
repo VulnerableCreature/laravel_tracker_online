6679ae687987c0dc6ec2b966c549476ae3073612
Leo <otselsiy@mail.ru>
2024-06-06 14:26:29 +0300

    feat: add export

--- src/resources/views/report/show.blade.php
+++ src/resources/views/report/show.blade.php
@@ -6,17 +6,30 @@
     Подразделение | {{ $division }} | {{ $date }}
 @endsection
 @section('content')
-    <div class="flex items-center justify-start gap-2 mb-5">
-        <a href="{{ route('report.index') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                <path fill-rule="evenodd" d="M17 10a.75.75 0 0 1-.75.75H5.612l4.158 3.96a.75.75 0 1 1-1.04 1.08l-5.5-5.25a.75.75 0 0 1 0-1.08l5.5-5.25a.75.75 0 1 1 1.04 1.08L5.612 9.25H16.25A.75.75 0 0 1 17 10Z" clip-rule="evenodd" />
-            </svg>
-        </a>
+    <div class="flex items-center justify-between gap-2 mb-5">
+        <div class="flex items-center justify-start">
+            <a href="{{ route('report.index') }}">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <path fill-rule="evenodd"
+                          d="M17 10a.75.75 0 0 1-.75.75H5.612l4.158 3.96a.75.75 0 1 1-1.04 1.08l-5.5-5.25a.75.75 0 0 1 0-1.08l5.5-5.25a.75.75 0 1 1 1.04 1.08L5.612 9.25H16.25A.75.75 0 0 1 17 10Z"
+                          clip-rule="evenodd"/>
+                </svg>
+            </a>
 
-        <span class="text-lg font-bold tracking-tight text-gray-900">Подразделение | {{ $division }} | {{ $date }}</span>
+            <span
+                class="text-lg font-bold tracking-tight text-gray-900">Подразделение | {{ $division }} | {{ $date }}</span>
+        </div>
+        @php
+            $title = "Список посещений. $division - $date";
+        @endphp
+        <button class="px-4 h-8 bg-black rounded-lg text-white font-medium text-lg mb-2"
+                onclick="exportTable('reports', '{{ $title }}')"
+                title="Экспортировать данные в .xlsx файл">
+            Экспорт
+        </button>
     </div>
     <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <table id="reports" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
@@ -50,5 +63,22 @@
             </tbody>
         </table>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
+    <script>
+        function exportTable(tableId, filename = '') {
+            const table = document.getElementById(tableId)
+            const wb = XLSX.utils.table_to_book(table, {sheet: 'Sheet 1'})
+            const wbout = XLSX.write(wb, {bookType: 'xlsx', type: 'binary'})
 
+            function s2ab(s) {
+                const buf = new ArrayBuffer(s.length)
+                const view = new Uint8Array(buf)
+                for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF
+                return buf
+            }
+
+            saveAs(new Blob([s2ab(wbout)], {type: 'application/octet-stream'}), filename + '.xlsx')
+        }
+    </script>
 @endsection
